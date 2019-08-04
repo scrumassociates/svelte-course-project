d9f3d66f5793951eb496b8399202d08693f4a1e2
Dan Moran <scrumassociates@gmail.com>
2019-08-04 16:24:58 -0400

    added a custom data store for meetups

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,15 @@
 <script>
+  import meetups from "./Meetups/meetups-store.js";
   import Header from "./UI/Header.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import TextInput from "./UI/TextInput.svelte";
   import Button from "./UI/Button.svelte";
   import EditMeetup from "./Meetups/EditMeetup.svelte";
 
-  let meetups = [
-    {
-      id: "m1",
-      title: "My cool meetup 1",
-      subtitle: "A really cool subtitle for meetup 1",
-      description: "We're gonna have a really good time at meetup 1!",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Wandsworth_Bridge_Road_Caff%C3%A8_Nero.jpg/1024px-Wandsworth_Bridge_Road_Caff%C3%A8_Nero.jpg",
-      address: "123 Main St., New York, NY 34567",
-      contactEmail: "meetup1@meetus.com",
-      isFavorite: false
-    },
-    {
-      id: "m2",
-      title: "My cool meetup 2",
-      subtitle: "A really cool subtitle for meetup 2",
-      description: "We're gonna have a really good time at meetup 2!",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Starbuckscenter.jpg/1024px-Starbuckscenter.jpg",
-      address: "456 Main St., New York, NY 34567",
-      contactEmail: "meetup2@meetus.com",
-      isFavorite: false
-    }
-  ];
-
   let editMode;
 
   function addMeetup(event) {
-    const newMeetup = {
-      id: Math.random().toString(),
+    const meetupData = {
       title: event.detail.title,
       subtitle: event.detail.subtitle,
       description: event.detail.description,
@@ -42,18 +17,13 @@
       contactEmail: event.detail.email,
       address: event.detail.address
     };
-    meetups = [newMeetup, ...meetups];
+    meetups.addMeetup(meetupData);
     editMode = null;
   }
 
   function toggleFavorite(event) {
     const id = event.detail;
-    const updatedMeetup = { ...meetups.find(m => m.id === id) };
-    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
-    const meetupIndex = meetups.findIndex(m => m.id === id);
-    const updatedMeetups = [...meetups];
-    updatedMeetups[meetupIndex] = updatedMeetup;
-    meetups = updatedMeetups;
+    meetups.toggleFavorite(id);
   }
 
   function cancelEdit() {
@@ -79,6 +49,6 @@
   {#if editMode === 'add'}
     <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
   {/if}
-  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+  <MeetupGrid meetups={$meetups} on:togglefavorite={toggleFavorite} />
 
 </main>
---
+++ src/Meetups/meetups-store.js
@@ -0,0 +1,52 @@
+import { writable } from "svelte/store"
+
+const meetups = writable([
+  {
+    id: "m1",
+    title: "My cool meetup 1",
+    subtitle: "A really cool subtitle for meetup 1",
+    description: "We're gonna have a really good time at meetup 1!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Wandsworth_Bridge_Road_Caff%C3%A8_Nero.jpg/1024px-Wandsworth_Bridge_Road_Caff%C3%A8_Nero.jpg",
+    address: "123 Main St., New York, NY 34567",
+    contactEmail: "meetup1@meetus.com",
+    isFavorite: false
+  },
+  {
+    id: "m2",
+    title: "My cool meetup 2",
+    subtitle: "A really cool subtitle for meetup 2",
+    description: "We're gonna have a really good time at meetup 2!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Starbuckscenter.jpg/1024px-Starbuckscenter.jpg",
+    address: "456 Main St., New York, NY 34567",
+    contactEmail: "meetup2@meetus.com",
+    isFavorite: false
+  }
+])
+
+const customMeetupsStore = {
+  subscribe: meetups.subscribe,
+  addMeetup: meetupData => {
+    const newMeetup = {
+      ...meetupData,
+      id: Math.random().toString(),
+      isFavorite: false
+    }
+    meetups.update(meetupList => {
+      return [newMeetup, ...meetupList]
+    })
+  },
+  toggleFavorite: id => {
+    meetups.update(meetupList => {
+      const updatedMeetup = { ...meetupList.find(m => m.id === id) }
+      updatedMeetup.isFavorite = !updatedMeetup.isFavorite
+      const meetupIndex = meetupList.findIndex(m => m.id === id)
+      const updatedMeetups = [...meetupList]
+      updatedMeetups[meetupIndex] = updatedMeetup
+      return updatedMeetups
+    })
+  }
+}
+
+export default customMeetupsStore
